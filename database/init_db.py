e4034a5aae4624ecf51af9f4c24c67a4a15be72b
Henrique Hideaki <hideakikoga.hk@gmail.com>
2022-11-02 18:40:41 -0300

    02_11

---
+++ database/init_db.py
@@ -0,0 +1,8 @@
+from database.connection import engine
+from database.models import Base
+
+async def create_database():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.drop_all)
+        await connection.run_sync(Base.metadata.create_all)
+
